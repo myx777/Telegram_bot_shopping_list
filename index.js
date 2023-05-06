c73f5a3f9a73a2846fc176f82eeaa8f80a8ffd95
MYX <it.anton22@gmail.com>
2023-05-06 23:24:49 +0300

    this version not correct

--- index.js
+++ index.js
@@ -19,6 +19,17 @@ if (!shoppingList[chatId]) {
 shoppingList[chatId].push(item);
 }; 
 
+// функция удаления товаров из списка покупок
+function remoteItem(chatId, index) {
+  if (shoppingList[chatId] && shoppingList[chatId][index]) {
+    shoppingList[chatId].splice[index, 1];
+  }
+
+};
+
+
+// добавляю  кнопку
+
 
 // старт бота
 bot.onText(/\/start/, (msg) => {
@@ -30,22 +41,49 @@ bot.onText(/\/start/, (msg) => {
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
   const item = msg.text;
-  let numberId = 0;
-  let i = 0;
-// проверка для того, чтобы команды в список не добавлялись 
+
+  // проверка для того, чтобы команды в список не добавлялись 
   if (item != '/start' && item != '/clear') {
-  addItemToShoppingList(chatId, item);
+    addItemToShoppingList(chatId, item);
+
   // второй вариант вывода списка покупок с нумерацией c помощью arr.map(function(item, index, array)
-    bot.sendMessage(chatId, `${shoppingList[chatId].map((item, index) => `${index + 1}. ${item}`).join('\n')}`);
+    bot.sendMessage(chatId, `${shoppingList[chatId].map((item, index) => `${index + 1}. ${item}`).join('\n')}`, {
+      // добавляю кнопку
+      reply_markup: {
+        inline_keyboard: shoppingList[chatId].map((item, index) => [
+          { text: '✅', callback_data: `/done ${index + 1}` },
+        ]),
+
+      },
+
+    });
 
   } else if (item === '/clear') {
-// очистить список
-  shoppingList[chatId].length = 0;
-  bot.sendMessage(chatId, `Список удален!`);
+  // очистить список
+    shoppingList[chatId].length = 0;
+   bot.sendMessage(chatId, `Список удален!`);
   }
 
 });
 
+// колл бек для кнопки
+bot.on('callback_query', (callbackQuery) => {
+  const chatId = callbackQuery.message.chat.id;
+  const messageId = callbackQuery.message.message_id;
+  const index = parseInt(callbackQuery.data.split(' ')[1]) - 1; // получаем индекс элемента
 
+  // удаляем элемент из списка
+  remoteItem(chatId, index);
 
-
+  // обновляем сообщение со списком покупок
+  bot.editMessageText(`${shoppingList[chatId].map((item, index) => `${index + 1}. ${item}`).join('\n')}`, {
+    chat_id: chatId,
+    message_id: messageId,
+    // обновляем кнопки для каждого элемента списка
+    reply_markup: {
+      inline_keyboard: shoppingList[chatId].map((item, index) => [
+        { text: '✅', callback_data: `/done ${index + 1}` },
+      ]),
+    },
+        });
+        });
